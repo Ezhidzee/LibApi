c03c54eb54136358cf51b84e09085b6742fcc26a
Artem <atrem2004atrem@gmail.com>
2025-02-07 21:53:29 +0300

    Renamed project

--- src/main/java/su/ezhidze/libapi/LibApiApplication.java
+++ src/main/java/su/ezhidze/libapi/LibApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibapiApplication {
+public class LibApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibapiApplication.class, args);
+		SpringApplication.run(LibApiApplication.class, args);
 	}
 
 }
